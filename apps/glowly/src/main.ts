286cb96914cd2c509da3188ac94460e566c3625f
Nursuni <202450493@jbnu.ac.kr>
2026-02-13 07:53:34 +0900

    feat: Implemented HTTP response compression (Gzip) to optimize API performance and reduce payload size.

--- apps/glowly/src/main.ts
+++ apps/glowly/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/logging.interceptor';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
+  app.use(compression());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
