499a4469c35b25e7f430e22a68f169a7f607b794
Nursuni <202450493@jbnu.ac.kr>
2026-02-13 07:54:45 +0900

    feat(security): add helmet middleware for HTTP security headers

--- apps/glowly/src/main.ts
+++ apps/glowly/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/logging.interceptor';
 import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.use(compression());
+  app.use(helmet()); //protects against common HTTP vulnerabilities.
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
