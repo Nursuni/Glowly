560f643d27bd2cd5c83cad1d69e2a0c2e251f1a0
Nursuni <202450493@jbnu.ac.kr>
2026-02-18 19:21:13 +0900

    feat: create follow related resolver and service model

--- apps/glowly/src/components/follow/follow.module.ts
+++ apps/glowly/src/components/follow/follow.module.ts
@@ -1,4 +1,23 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { FollowResolver } from './follow.resolver';
+import { FollowService } from './follow.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MemberModule } from '../member/member.module';
+import { AuthModule } from '../auth/auth.module';
+import FollowSchema from '../../libs/schema/Follow.model';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Follow',
+        schema: FollowSchema,
+      },
+    ]),
+    AuthModule,
+    forwardRef(() => MemberModule),
+  ],
+  providers: [FollowResolver, FollowService],
+  exports: [FollowService],
+})
 export class FollowModule {}
