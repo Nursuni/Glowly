4d03aad430acad92a81ca920b70452c77ea0855a
Nursuni <202450493@jbnu.ac.kr>
2026-02-21 23:04:00 +0900

    fix: modify batch module and connect to MongoDb

--- apps/batch/src/batch.module.ts
+++ apps/batch/src/batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BatchController } from './batch.controller';
 import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [BatchController],
   providers: [BatchService],
 })
---
+++ apps/batch/src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        //database+ backend ulash
+        uri:
+          process.env.NODE_ENV === 'production'
+            ? process.env.MONGO_PROD
+            : process.env.MONGO_DEV,
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {
+  constructor(@InjectConnection() private readonly connection: Connection) {
+    //backend+database ulanganligini tekshirish
+    if (connection.readyState === 1) {
+      console.log('connection.readyState', connection.readyState);
+      console.log(
+        `MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`,
+      );
+    } else {
+      console.log('DB is not connected');
+    }
+  }
+}
